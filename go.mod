a9bc0220beb4565ab7491f43af09019a333ff9cb
jjcapellan <soycape@hotmail.com>
2021-08-20 12:44:36 +0200

    Change:used module jjc-reverse-proxy as proxy

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/jjcapellan/jjc-reverse-proxy v0.1.0-alpha
 	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -1,33 +1,52 @@
 package main
 
 import (
+	"log"
 	"os"
 	"strconv"
 	"sync"
 
+	"github.com/joho/godotenv"
+
 	backend "github.com/jjcapellan/go-proxy-example/backend"
 	frontend "github.com/jjcapellan/go-proxy-example/frontend"
-	proxy "github.com/jjcapellan/go-proxy-example/proxy"
+	proxy "github.com/jjcapellan/jjc-reverse-proxy"
 )
 
 const PORT_LIMIT int = 65535
 
-var PORT_PROXY string
+var PORT_PROXY string // From environment
 var PORT_FRONTEND string
-var PORT_BACKEND string
+var PORT_API string
+
+const API_ROUTE string = "/api"
 
 func main() {
 
+	loadEnvironment()
 	setupPorts()
 
 	var wg sync.WaitGroup
-
-	go backend.Init(PORT_BACKEND)
-	go frontend.Init(PORT_FRONTEND)
-	go proxy.Init(PORT_PROXY, PORT_FRONTEND, PORT_BACKEND)
-
 	wg.Add(1)
+
+	go backend.Init(PORT_API)
+	go frontend.Init(PORT_FRONTEND)
+
+	proxyConfig := getProxyConfig()
+	proxy.Setup(proxyConfig)
+	go func() {
+		defer wg.Done()
+		proxy.Start()
+	}()
+
 	wg.Wait()
+}
+
+func loadEnvironment() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Main: environment file not found")
+	}
 }
 
 func setupPorts() {
@@ -40,7 +59,7 @@ func setupPorts() {
 	}
 
 	PORT_FRONTEND = strconv.Itoa(p + 1)
-	PORT_BACKEND = strconv.Itoa(p + 2)
+	PORT_API = strconv.Itoa(p + 2)
 }
 
 func getPortProxy() string {
@@ -51,4 +70,15 @@ func getPortProxy() string {
 		port = os.Getenv("PORT")
 	}
 	return port
+}
+
+func getProxyConfig() proxy.Config {
+	c := proxy.Config{
+		PortProxy:    PORT_PROXY,
+		PortApi:      PORT_API,
+		PortFrontend: PORT_FRONTEND,
+		ApiRoute:     API_ROUTE,
+		ApiKey:       os.Getenv("API_KEY"),
+	}
+	return c
 }
